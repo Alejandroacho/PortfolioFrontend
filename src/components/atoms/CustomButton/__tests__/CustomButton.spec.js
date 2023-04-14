4fb8c6b423738530716d6eb2ff29a8b977cfe10f
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2023-04-14 23:56:43 +0200

    feat: Add test skeleton to button

---
+++ src/components/atoms/CustomButton/__tests__/CustomButton.spec.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import SpinningLoader from "../SpinningLoader.vue";
+
+describe("CustomButton", () => {
+});
