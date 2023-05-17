4bea003f478a513405d209c19721ae2688e7c7e3
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2023-05-17 23:47:01 +0200

    chore: Add Project Section first test

---
+++ src/components/molecules/ProjectSection/__tests__/ProjectSection.spec.js
@@ -0,0 +1,57 @@
+import { describe, it, expect } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import ProjectSection from "../ProjectSection.vue";
+
+describe("ProjectSection", () => {
+  it("renders properly", () => {
+    const project =  {
+      id: 1,
+      title: "Project 1",
+      description: "Project 1 description",
+      url: "https://www.google.com",
+      is_public: true,
+      repository: "https://www.github.com",
+      authors: [
+        {
+          id: 1,
+          first_name: "Author",
+          last_name: "Author",
+          social_networks: [
+            {
+              platform: "github",
+              url: "https://www.github.com",
+              nickname: "author",
+            },
+          ],
+        },
+      ],
+      images: [
+        {
+          id: 1,
+          description: "Image 1",
+          type: "PC",
+          image: "public/design-system-light.png",
+        },
+        {
+          id: 2,
+          description: "Image 2",
+          type: "PC",
+          image: "public/design-system-light.png",
+        },
+        {
+          id: 3,
+          description: "Image 3",
+          type: "OTHER",
+          image: "public/design-system-light.png",
+        },
+      ]
+    }
+    const wrapper = mount(ProjectSection, {
+      props: {
+        project: project
+      },
+    });
+  expect(wrapper.find("h2").text()).toBe(project.title);
+  });
+});
