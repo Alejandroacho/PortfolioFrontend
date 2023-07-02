2a0b065afb0bec9ba339b4bda875f599368b4bb1
Alejandro Acho <alejandro@tribes.studio>
2023-07-02 22:39:41 +0200

    refactor: Sliter

--- src/components/molecules/ImageSlider/ImageSlider.vue
+++ src/components/molecules/ImageSlider/ImageSlider.vue
@@ -1,124 +1,161 @@
 <template>
-    <div class="carousel">
-        <div class="carousel-inner">
-            <transition :name="transitionEffect" v-for="image in images" v-bind:key="images.id">
-                <div
-                    class="carousel-item"
-                    v-show="currentSlide === index"
-                >
-                    <ImageDisplay :image="image"/>
-                </div>
-            </transition>
+  <div class="carousel">
+    <button class="carousel-control left" @click="prev">Prev</button>
+    <div class="carousel-inner">
+      <transition
+        :name="transitionEffect"
+        v-for="(image, index) in images"
+        v-bind:key="index"
+      >
+        <div class="carousel-item" v-show="currentSlide === index">
+          <ImageDisplay :image="image" />
         </div>
+      </transition>
     </div>
+    <button class="carousel-control right" @click="next">Next</button>
+  </div>
 </template>
 
 <script>
 import ImageDisplay from "@/components/atoms/ImageDisplay/ImageDisplay.vue";
 
 export default {
-    name: "ImageSlider",
-    components: {ImageDisplay},
-    data: () => ({
-        currentSlide: 0,
-        slideInterval: null,
-        direction: "right",
-        images: ["https://picsum.photos/900/400", "https://picsum.photos/900/400"],
-        interval: 5000
-    }),
-    methods: {
-        setCurrentSlide(index) {
-            this.currentSlide = index;
-        },
-        prev(step = -1) {
-            const index =
-                this.currentSlide > 0
-                    ? this.currentSlide + step
-                    : this.slides.length - 1;
-            this.setCurrentSlide(index);
-            this.direction = "left";
-            this.startSlideTimer();
-        },
-        _next(step = 1) {
-            const index =
-                this.currentSlide < this.slides.length - 1
-                    ? this.currentSlide + step
-                    : 0;
-            this.setCurrentSlide(index);
-            this.direction = "right";
-        },
-        next(step = 1) {
-            this._next(step);
-            this.startSlideTimer();
-        },
-        startSlideTimer() {
-            this.stopSlideTimer();
-            this.slideInterval = setInterval(() => {
-                this._next();
-            }, this.interval);
-        },
-        stopSlideTimer() {
-            clearInterval(this.slideInterval);
-        },
-        switchSlide(index) {
-            const step = index - this.currentSlide;
-            if (step > 0) {
-                this.next(step);
-            } else {
-                this.prev(step);
-            }
-        },
+  name: "ImageSlider",
+  components: { ImageDisplay },
+
+  mounted() {
+    this.startSlideTimer();
+  },
+
+  data: () => ({
+    currentSlide: 0,
+    slideInterval: null,
+    direction: "right",
+    interval: 5000,
+  }),
+
+  props: {
+    images: {
+      type: Array,
+      required: true,
     },
-    mounted() {
-        this.startSlideTimer();
+  },
+
+  computed: {
+    transitionEffect() {
+      return this.direction === "right" ? "slide-out" : "slide-in";
     },
-    beforeUnmount() {
-        this.stopSlideTimer();
+  },
+
+  methods: {
+    setCurrentSlide(index) {
+      this.currentSlide = index;
     },
-    computed: {
-        transitionEffect() {
-            return this.direction === "right" ? "slide-out" : "slide-in";
-        },
+
+    prev() {
+      const index =
+        this.currentSlide > 0 ? this.currentSlide - 1 : this.images.length - 1;
+      this.setCurrentSlide(index);
+      this.direction = "left";
+      this.startSlideTimer();
     },
+
+    _next(step = 1) {
+      const index =
+        this.currentSlide < this.images.length - 1
+          ? this.currentSlide + step
+          : 0;
+      this.setCurrentSlide(index);
+      this.direction = "right";
+    },
+
+    next() {
+      this._next();
+      this.startSlideTimer();
+    },
+
+    startSlideTimer() {
+      this.stopSlideTimer();
+      this.slideInterval = setInterval(() => {
+        this._next();
+      }, this.interval);
+    },
+
+    stopSlideTimer() {
+      clearInterval(this.slideInterval);
+    },
+
+    switchSlide(index) {
+      const step = index - this.currentSlide;
+      if (step > 0) {
+        this.next(step);
+      } else {
+        this.prev(step);
+      }
+    },
+  },
+
+  beforeUnmount() {
+    this.stopSlideTimer();
+  },
 };
 </script>
 
 <style scoped>
 .carousel {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 .carousel-inner {
-    position: relative;
-    width: 900px;
-    height: 400px;
-    overflow: hidden;
+  position: relative;
+  width: 900px;
+  height: 400px;
+  overflow: hidden;
 }
 
-
 .carousel-item {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 .slide-in-enter-active,
 .slide-in-leave-active,
 .slide-out-enter-active,
 .slide-out-leave-active {
-    transition: all 1s ease-in-out;
+  transition: all 1s ease-in-out;
 }
 .slide-in-enter-from {
-    transform: translateX(-100%);
+  transform: translateX(-100%);
 }
 .slide-in-leave-to {
-    transform: translateX(100%);
+  transform: translateX(100%);
 }
 .slide-out-enter-from {
-    transform: translateX(100%);
+  transform: translateX(100%);
 }
 .slide-out-leave-to {
-    transform: translateX(-100%);
+  transform: translateX(-100%);
+}
+
+.carousel-control {
+  background-color: rgba(0, 0, 0, 0.5);
+  border: none;
+  display: inline-block;
+  position: absolute;
+  height: 50px;
+  width: 70px;
+  top: calc(50% - 25px);
+  color: #f3f3f3;
+  cursor: pointer;
+}
+.left {
+  z-index: 999;
+  left: 0;
+}
+.right {
+  z-index: 999;
+  right: 0;
 }
 </style>
