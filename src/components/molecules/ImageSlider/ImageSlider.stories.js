919d06994e71f97b90ef128e0a1b50a32bec9a0a
Alejandro Acho <alejandro@tribes.studio>
2023-07-02 22:39:35 +0200

    feat: Refactor slider story

--- src/components/molecules/ImageSlider/ImageSlider.stories.js
+++ src/components/molecules/ImageSlider/ImageSlider.stories.js
@@ -1,37 +1,55 @@
 import ImageSlider from "./ImageSlider.vue";
 import generateSource from "../../../../.storybook/utils.js";
 export default {
-    title: "molecules/ImageSlider",
-    component: ImageSlider,
-    parameters: {
-        docs: {
-            description: {
-                component: "Content loader, to show while the content is loading.",
-            },
-        },
+  title: "molecules/ImageSlider",
+  component: ImageSlider,
+  parameters: {
+    docs: {
+      description: {
+        component: "Image slider component.",
+      },
     },
-    argTypes: {
-        message: {
-            description: "Message displayed while loading.",
-        },
+  },
+  argTypes: {
+    images: {
+      description: "List of images that will be in the slider.",
     },
+  },
 };
 
 const codeTemplate = '<ImageSlider v-bind="args"/>';
 
 const Template = (args) => ({
-    components: { ImageSlider },
-    setup() {
-        return { args };
-    },
-    template: codeTemplate,
+  components: { ImageSlider },
+  setup() {
+    return { args };
+  },
+  template: codeTemplate,
 });
 
 export const ImageSliderStory = Template.bind({});
 
 ImageSliderStory.parameters = {
-    docs: {
-        source: { code: generateSource(codeTemplate, {}) },
-    },
+  docs: {
+    source: { code: generateSource(codeTemplate, {}) },
+  },
 };
-ImageSliderStory.args = {};
+ImageSliderStory.args = {
+  images: [
+    {
+      image: "public/design-system-light.png",
+      description: "JavaScript",
+      type: "PC",
+    },
+    {
+      image: "public/design-system-dark.png",
+      description: "JavaScript",
+      type: "PC",
+    },
+    {
+      image: "public/circular-logo.png",
+      description: "JavaScript",
+      type: "PC",
+    },
+  ],
+};
