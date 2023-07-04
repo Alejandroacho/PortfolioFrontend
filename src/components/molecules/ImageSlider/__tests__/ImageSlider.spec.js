7039e382358ce1cbf5a19f713916a57fe193118c
Alejandro Acho <alejandroacho@hotmail.com>
2023-07-04 23:46:44 +0200

    feat: Test renders correctly applied

--- src/components/molecules/ImageSlider/__tests__/ImageSlider.spec.js
+++ src/components/molecules/ImageSlider/__tests__/ImageSlider.spec.js
@@ -4,5 +4,32 @@ import { mount } from "@vue/test-utils";
 import ImageSlider from "../ImageSlider.vue";
 
 describe("ImageSlider", () => {
-
+  it("renders properly", () => {
+    const images = [
+      {
+        id: 1,
+        description: "Image 1",
+        type: "PC",
+        image: "public/design-system-light.png",
+      },
+      {
+        id: 2,
+        description: "Image 2",
+        type: "PC",
+        image: "public/design-system-light.png",
+      },
+      {
+        id: 3,
+        description: "Image 3",
+        type: "PC",
+        image: "public/design-system-light.png",
+      },
+    ];
+    const wrapper = mount(ImageSlider, {
+      props: {
+        images: images,
+      },
+    });
+    expect(wrapper.findAll("img").length).toBe(images.length);
+  });
 });
