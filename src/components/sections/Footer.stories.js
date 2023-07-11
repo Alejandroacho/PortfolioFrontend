601332dba9a031d8dbb489aff8880f99631e0c3f
Alejandro Acho <alejandroacho@hotmail.com>
2023-07-11 22:45:14 +0200

    feat: Create footer skeleton

---
+++ src/components/sections/Footer.stories.js
@@ -0,0 +1,31 @@
+import Footer from "./Footer.vue";
+import generateSource from "../../../.storybook/utils.js";
+export default {
+  title: "sections/Footer",
+  component: Footer,
+  parameters: {
+    docs: {
+      description: {
+        component: "Footer section component.",
+      },
+    },
+  },
+};
+
+const codeTemplate = '<Footer v-bind="args"/>';
+
+const Template = (args) => ({
+  components: { Footer },
+  setup() {
+    return { args };
+  },
+  template: codeTemplate,
+});
+
+export const FooterStory = Template.bind({});
+
+FooterStory.parameters = {
+  docs: {
+    source: { code: generateSource(codeTemplate, {}) },
+  },
+};
---
+++ src/components/sections/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer>
+    <div class="footer-container">
+      <div class="column">
+        <!-- Add content for the first column here -->
+      </div>
+      <div class="column">
+        <!-- Add content for the second column here -->
+      </div>
+      <div class="column">
+        <!-- Add content for the third column here -->
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterSection",
+  // Include any data, computed properties, methods, or lifecycle hooks needed here
+}
+</script>
+
+<style scoped>
+.footer-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.column {
+  flex: 1;
+  padding: 10px;
+  /* Add any additional styling needed for the columns here */
+}
+</style>
