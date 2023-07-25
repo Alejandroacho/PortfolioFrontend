1feed87603d58e18bb366455b013e08aaebce0f5
Alejandro Acho <alejandroacho@hotmail.com>
2023-07-25 07:09:48 +0200

    feat: Add simple hero component

---
+++ src/components/sections/Hero/Hero.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hero" id="droplet-container">
+    <img
+      src="../../../../public/logo.png"
+      alt="Logo image"
+      draggable="false"
+      class="logo"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroSection",
+};
+</script>
+
+<style scoped lang="scss">
+@import "src/assets/colors.scss";
+
+.hero{
+  background-color: $background-gray;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.logo{
+  width: 50vh;
+  height: 50vh;
+  border-radius: 100%;
+}
+</style>
