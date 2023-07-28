3ba354311965db7891956f000f6b85649c55b8ab
Alejandro Acho <alejandroacho@hotmail.com>
2023-07-28 19:52:39 +0200

    feaT: First iteration of drop

--- src/components/sections/Hero/Hero.vue
+++ src/components/sections/Hero/Hero.vue
@@ -8,18 +8,90 @@
     />
     <div class="drop"></div>
   </div>
+    <svg
+      width="100%"
+      height="100px"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="none"
+      class="wave"
+    >
+      <polygon
+        :points="wavePoints"
+        fill="white"
+        transform="translate(0, 50)"
+      />
+    </svg>
 </template>
 
 <script>
+import { WaveTank } from "./WaveTank.js";
 export default {
   name: "HeroSection",
+  data() {
+    return {
+      waveTank: new WaveTank(),
+      SVG_WIDTH: 100,
+      width: 100,
+      widthRef: null,
+      springs: null,
+      requestId: null,
+      grid: null,
+      springsPath: null,
+      requestIdRef: null,
+      wavePoints: null
+    };
+  },
+  methods: {
+    update(timestamp) {
+      this.waveTank.update(this.waveTank.springs);
+      this.springs = [...this.waveTank.springs];
+      this.wavePoints = [
+        [0, 100],
+        [0, 0],
+        ...this.springs.map((x, i) => [i * this.grid, x.p]),
+        [this.width, 0],
+        [this.width, 100],
+      ];
+      const offset = 500;
+      const saw = (timestamp + offset) / 2000 - Math.floor((timestamp + offset) / 2000);
+      if (saw < 0.01) {
+        this.drop();
+      }
+      this.requestId = requestAnimationFrame(this.update);
+    },
+    resize() {
+      this.width = document.body.clientWidth;
+    },
+    drop() {
+      const dropPosition = 50;
+      this.waveTank.springs[dropPosition].p = -50;
+    },
+  },
+  mounted() {
+    this.resize();
+    this.springs = this.waveTank.springs;
+    this.grid = this.SVG_WIDTH / this.waveTank.waveLength;
+    const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+    if (mediaQuery.matches) return;
+    if (this.requestIdRef !== undefined) {
+      cancelAnimationFrame(this.requestIdRef);
+    }
+    this.requestIdRef = requestAnimationFrame(this.update);
+    window.addEventListener("resize", this.resize);
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.resize);
+    if (this.requestId !== undefined) {
+      cancelAnimationFrame(this.requestId);
+    }
+  },
 };
 </script>
 
 <style scoped lang="scss">
 @import "src/assets/colors.scss";
 
-.hero{
+.hero {
   background-color: $background-gray;
   height: 100vh;
   display: flex;
@@ -27,28 +99,28 @@ export default {
   justify-content: center;
   flex-direction: column;
 }
-.logo{
-  width: 50vh;
-  height: 50vh;
+.logo {
+  width: 30vh;
+  height: 30vh;
   border-radius: 100%;
 }
 
 .drop {
   position: relative;
-	width: 20px;
-	height: 20px;
+  width: 20px;
+  height: 20px;
   top: -30px;
   margin: 0 auto;
-	background: #FFF;
-	-moz-border-radius: 20px;
-	-webkit-border-radius: 20px;
-	border-radius: 20px;
+  background: #fff;
+  -moz-border-radius: 20px;
+  -webkit-border-radius: 20px;
+  border-radius: 20px;
   -moz-animation-name: drip;
   -webkit-animation-name: drip;
   animation-name: drip;
-  -moz-animation-timing-function: cubic-bezier(1,0,.91,.19);
-  -webkit-animation-timing-function: cubic-bezier(1,0,.91,.19);
-  animation-timing-function: cubic-bezier(1,0,.91,.19);
+  -moz-animation-timing-function: cubic-bezier(1, 0, 0.91, 0.19);
+  -webkit-animation-timing-function: cubic-bezier(1, 0, 0.91, 0.19);
+  animation-timing-function: cubic-bezier(1, 0, 0.91, 0.19);
   -moz-animation-duration: 2s;
   -webkit-animation-duration: 2s;
   animation-duration: 2s;
@@ -61,10 +133,10 @@ export default {
   content: "";
   position: absolute;
   width: 0;
-	height: 0;
-	border-left: 10px solid transparent;
-	border-right: 10px solid transparent;
-	border-bottom: 30px solid rgba(255,255,255,1);
+  height: 0;
+  border-left: 10px solid transparent;
+  border-right: 10px solid transparent;
+  border-bottom: 30px solid rgba(255, 255, 255, 1);
   top: -22px;
 }
 
@@ -98,8 +170,11 @@ export default {
 }
 
 @keyframes drip {
-    to {
-      top: 190px;
-    }
+  to {
+    top: 190px;
+  }
+}
+.wave{
+  margin-top: -55px;
 }
 </style>
