91a3e7ee4d672a3b5dfabd1e96de64bb6c10af26
Alejandro Acho <alejandroacho@hotmail.com>
2023-08-17 08:22:01 +0200

    chore: Move spinning loader to TS

--- src/components/atoms/SpinningLoader/__tests__/Loader.spec.ts
+++ src/components/atoms/SpinningLoader/__tests__/Loader.spec.ts
@@ -1,12 +1,12 @@
 import { describe, it, expect } from "vitest";
-
 import { mount } from "@vue/test-utils";
 import SpinningLoader from "../SpinningLoader.vue";
+import {VueWrapper} from "@vue/test-utils/dist/vueWrapper";
 
-describe("ImageDisplay", () => {
-  it("renders properly with message", () => {
-    const loadingMessage = "Items are loading...";
-    const wrapper = mount(SpinningLoader, {
+describe("ImageDisplay", (): void => {
+  it("renders properly with message", (): void => {
+    const loadingMessage: string = "Items are loading...";
+    const wrapper: VueWrapper = mount(SpinningLoader, {
       props: {
         message: loadingMessage,
       },
@@ -15,10 +15,8 @@ describe("ImageDisplay", () => {
     expect(wrapper.find("h3").text()).toBe(loadingMessage);
   });
 
-  it("renders properly with default message", () => {
-    const loadingMessage = "Content is loading...";
-    const wrapper = mount(SpinningLoader, { props: {} });
-    expect(wrapper.props().message).toBe(loadingMessage);
-    expect(wrapper.find("h3").text()).toBe(loadingMessage);
+  it("renders properly with default message", (): void => {
+    const wrapper: VueWrapper = mount(SpinningLoader, { props: {} });
+    expect(wrapper.props().message).toBeUndefined();
   });
 });
