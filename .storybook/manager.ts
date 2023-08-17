5d7e03bc70f5bab9df3a9c68078f768e66dc668a
Alejandro Acho <alejandroacho@hotmail.com>
2023-08-17 07:53:43 +0200

    chore: move storybook to TS

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,4 +1,4 @@
-const link = document.createElement('link');
+const link: HTMLLinkElement = document.createElement('link');
 link.setAttribute('rel', 'shortcut icon');
 link.setAttribute('href', '/favicon.ico');
 document.head.appendChild(link);
--- .storybook/utils.ts
+++ .storybook/utils.ts
@@ -1,4 +1,4 @@
-const stringifyArguments = (key, value) => {
+const stringifyArguments = (key: string, value: string): string => {
   switch (typeof value) {
   case 'string':
       return `${key}="${value}"`;
@@ -9,7 +9,7 @@ const stringifyArguments = (key, value) => {
   }
 };
 
-const generateSource = (templateSource, args) => {
+const generateSource = (templateSource: string, args: Object): string => {
   const stringifiedArguments = Object.keys(args).map(
     (key) => stringifyArguments(key, args[key])
   ).join(' ');
